3a170225915f453ef40baaded59fee65e37878c7
sivins <sean.ivins@yahoo.com>
2015-04-23 22:20:48 -0500

    Fixed bugs

--- AwesomeTasks/Models/Todo.cs
+++ AwesomeTasks/Models/Todo.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace AwesomeTasks.Models
 {
-    public class Task
+    public class Todo
     {
-        [Key]
         public int ID { get; set; }
 
-        [StringLength(1000)]
         public string Description { get; set; }
 
         public DateTime TimeStamp { get; set; }
